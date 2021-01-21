21dffc8c7d99113f2c8b294a91674327882a725c
Nadish Maredia <Nadish Maredia@NADISH>
2021-01-21 23:13:00 +0500

    Add Endpoints

--- tinder-backend/server.js
+++ tinder-backend/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const Cards = require('./dbCards');
+const Cors = require('cors')
 //App Config
 const app = express();
 const port = process.env.PORT || 8001;
 const connection_url = 'mongodb+srv://admin:nYrPFSyHYAYPqQcb@cluster0.adelv.mongodb.net/tinderdb?retryWrites=true&w=majority';
 
 //Middlewares
-
+app.use(express.json());
+app.use(Cors());
 //DB Config
 mongoose.connect(connection_url, {
     useNewUrlParser: true,
@@ -20,6 +22,28 @@ mongoose.connect(connection_url, {
 //API Endpoints
 app.get('/', (req, res) => {
     res.status(200).send('Hello from node');
+});
+
+app.get('/tinder/card', (req, res) => {
+    Cards.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(data);
+        }
+    });
+});
+
+app.post('/tinder/card', (req, res) => {
+    const dbCards = req.body;
+
+    Cards.create(dbCards, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(201).send(data);
+        }
+    });
 });
 
 //Listener
